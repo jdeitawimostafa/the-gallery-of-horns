0f73fa1965d9e23bb9600a76a3fa42ac7613f472
mostafakjdeitawi <mostafagedo0785@gmail.com>
2021-04-02 22:44:58 +0300

    editing on filter function and repeation

--- js/app.js
+++ js/app.js
@@ -1,32 +1,41 @@
 'use strict';
 
 
-function rendertemplate() {
     $.ajax('./data/page1.json').then(galleryData =>{
     galleryData.forEach(items =>{
-        $('section').show();
         let newItem = new galleryItem(items);
         newItem.renderNewItem();
     });
+    editiedKeywords();
     $('#photo-template').first().remove();
 });
-}
-rendertemplate();
+
 let keydowrds = [];
 function galleryItem(key){
     this.title = key.title;
     this.image = key.image_url;
-    this.description =key.description;
+    this.description = key.description;
     this.keyword = key.keyword;
 
     if(!keydowrds.includes(this.keyword)){
-        keydowrds.push(this.keyword);
+        keydowrds.push(this.keyword)
     }
+
+   
 }
 
+function editiedKeywords(){
+    for ( let i = 0 ; i<keydowrds.length; i++){
+        let options = $('option').first().clone().text(keydowrds[i]);
+        $('select').append(options);
+        // options.attr('value',keydowrds[i]);
+    }
+    console.log(keydowrds);
+
+}
+
+
 galleryItem.prototype.renderNewItem = function(){
-    let keywordsList = $('option').first().clone().text(this.keyword);
-    $('select').append(keywordsList);
     let sectionCopy = $('#photo-template').first().clone();
     sectionCopy.addClass(this.keyword);
     $('main').append(sectionCopy);
@@ -35,10 +44,19 @@ galleryItem.prototype.renderNewItem = function(){
     sectionCopy.find('p').text(this.description);
 }
 
+$('select').change(function(){
+    let selectedKey = $(this).val();
+    console.log(selectedKey);
+    $('section').hide();
+    $(`.${selectedKey}`).show();
+});
 
-    $('select').change(function(){
-        let selectedKey = $(this).val();
-        console.log(selectedKey);
-        $('section').hide();
-        $(`.${selectedKey}`).show();
-    });
+// another way to prevent repeation
+// function options(array){
+//     let preventRepeat = [... new Set(array)];  console.log(preventRepeat);
+//     preventRepeat.forEach(    element =>{
+//         $('select').append(`<option value="${element}">${element}</option>`);
+//       }  );
+//   }
+
+
